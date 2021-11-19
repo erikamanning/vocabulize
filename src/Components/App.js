56d5e192d6d3ba3172b5b2a579e3e43f83f5971c
erika <erikamanning@protonmail.com>
2021-11-19 13:17:04 -0700

    added application state and functionality to get api data on load

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import RandomWordsAPI from './RandomWordsAPI';
+import Navigation from './Navigation'
+import React, {useEffect,useState} from "react"
+
+const DeckContext = React.createContext();
+
 
 function App() {
+
+  const [deck, setDeck] = useState([]);
+
+  useEffect(()=>{
+
+    async function getWords(){
+
+      let words = await RandomWordsAPI.getWords();
+      console.log('Word: ', words);
+      setDeck(words);
+    }
+
+    if(deck && deck.length === 0){
+      getWords();
+    }
+
+  },[deck]);
+
+  console.log('Deck: ', deck);
+
   return (
     <div className="App">
-      <h1>Welcome to Vocabulize</h1>
+      <DeckContext.Provider value={{deck}}>
+        <h1>Welcome to Vocabulize</h1>
+        <Navigation />
+      </DeckContext.Provider>
     </div>
   );
 }
 
+export {DeckContext};
 export default App;
