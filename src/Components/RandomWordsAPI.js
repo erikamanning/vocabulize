fc0045b5ae611e34ac09ba460c1c651e347e7678
erika <erikamanning@protonmail.com>
2021-11-19 13:16:05 -0700

    added new function to get 10 words from api

--- src/Components/RandomWordsAPI.js
+++ src/Components/RandomWordsAPI.js
@@ -3,27 +3,12 @@ const BASE_URL = 'https://random-words-api.vercel.app'
 
 class RandomWordsAPI{
 
-    static async request(endpoint,data={},method="get"){
-
-        const url = `${BASE_URL}/${endpoint}`;
-        const params = (method === "get")
-        ? data
-        : {};
-
-        try{
-            return (await axios({url, method, data, params})).data;
-        }
-        catch(error){
-            console.error("Random-Words-API Error:", error.response);
-            let message = error.response.data.error.message;
-            throw Array.isArray(message) ? message : [message];
-        }
-    }
-
     static async getWord(){
+        console.log('getting word...');
         try{
 
-            let res = await this.request('word');
+            let res = await axios.get(`${BASE_URL}/word`);
+            console.log('res: ', res);
             return res.data;
         }
         catch(error){
@@ -33,7 +18,26 @@ class RandomWordsAPI{
         }
 
     }
+    static async getWords(numWords =10){
+        console.log(`getting ${numWords} words...`);
+        let words = [];
+        let promises = [];
 
+        for(let i=0; i<numWords; i++){
+            promises.push(await axios.get(`${BASE_URL}/word`));
+        }
+
+        Promise.all(promises).then((values)=>{
+
+            for(let value of values){
+                words.push(value.data[0]);
+            }
+            console.log('words: ', words);
+        });
+
+        return words;
+
+    }
 }
 
 export default RandomWordsAPI;
