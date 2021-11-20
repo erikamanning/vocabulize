5bc5b9ef82ee34007c5e40c206259ab1aafa375c
erika <erikamanning@protonmail.com>
2021-11-19 17:44:34 -0700

    added functionality to show canvas with button click

--- src/Components/Deck/Card/CardBack/CardBack.js
+++ src/Components/Deck/Card/CardBack/CardBack.js
@@ -1,21 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
 import './CardBack.css'
+import CardCanvas from "./CardCanvas";
 
 const CardBack = () => {
 
     const drawing = true;
+    const [showCanvas, setShowCanvas] = useState(false);
+
+
+    const accessCanvas = () => {
+        setShowCanvas(sc=>!sc);
+    }
 
     return (
         <div>
             <h1>CardBack</h1>
 
             {
-                drawing
+                drawing || showCanvas
                 ? <div>
                     <h5>Your Drawing:</h5>
                     <img className='CardBack-drawing' src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F10%2F09%2F228182-bunnies-animals-rabbits.jpg&f=1&nofb=1" alt="bunny" />
                   </div>
-                : <p>No drawing yet!</p>
+                : <CardCanvas/>
             }
             <div className='CardBack-button-container'>            
                 {
