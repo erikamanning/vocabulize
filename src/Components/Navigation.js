fbd129ce4224b81631f7422ebccd0dc98f0c56fd
erika <erikamanning@protonmail.com>
2021-11-19 17:02:33 -0700

    added home link to navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { Outlet, Link } from 'react-router-dom';
+import './Navigation.css'
 
 const Navigation = () => {
 
         return (
-            <div className='Navigation'>
-                <Link to="/deck">Deck</Link> |{' '}
-                <Link to="/quiz">Quiz</Link>
+            <div>
+                <div className="Navigation-links">
+                  <Link to="/">Home</Link> |{' '}
+                  <Link to="/deck">Deck</Link> |{' '}
+                  <Link to="/quiz">Quiz</Link>
+                </div>
               <Outlet />
             </div>
         )
