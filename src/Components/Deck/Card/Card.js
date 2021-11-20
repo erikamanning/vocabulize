2a44e7ebdbf9a039681d5f574388c8d812fca270
erika <erikamanning@protonmail.com>
2021-11-19 17:08:54 -0700

    added card flipping functionality -- no animation

--- src/Components/Deck/Card/Card.js
+++ src/Components/Deck/Card/Card.js
@@ -1,16 +1,32 @@
-import React from "react"
+import React, {useState} from "react"
 import './Card.css'
-import CardFront from './CardFront'
+import CardFront from './CardFront/CardFront'
+import CardBack from './CardBack/CardBack'
 
 const CardContext = React.createContext();
 
 const Card = ({card}) => {
 
+    const [cardSide, setCardSide] = useState('front');
+
+    const flipCard = ()=>{
+        if(cardSide === "front")
+            setCardSide("back")
+        else
+            setCardSide("front")
+
+    }
+
     return (
         <CardContext.Provider value={card}>
             <div className='Card'>
                 <h5 className='Card-title'>Card</h5>
-                <CardFront />
+                {
+                    cardSide === "front"
+                    ? <CardFront />
+                    : <CardBack />
+                }
+                <button onClick={flipCard}>Flip Card</button>
             </div>
         </CardContext.Provider>
     )
