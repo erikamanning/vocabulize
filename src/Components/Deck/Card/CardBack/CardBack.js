376797eb8cc7f68c4c6b8af2c5e6950b10f18229
erika <erikamanning@protonmail.com>
2021-11-19 17:25:11 -0700

    added filler data and buttons

--- src/Components/Deck/Card/CardBack/CardBack.js
+++ src/Components/Deck/Card/CardBack/CardBack.js
@@ -1,11 +1,29 @@
 import React from "react"
-
+import './CardBack.css'
 
 const CardBack = () => {
+
+    const drawing = true;
 
     return (
         <div>
             <h1>CardBack</h1>
+
+            {
+                drawing
+                ? <div>
+                    <h5>Your Drawing:</h5>
+                    <img className='CardBack-drawing' src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F10%2F09%2F228182-bunnies-animals-rabbits.jpg&f=1&nofb=1" alt="bunny" />
+                  </div>
+                : <p>No drawing yet!</p>
+            }
+            <div className='CardBack-button-container'>            
+                {
+                    drawing 
+                    ? <button>Update</button>
+                    : <button>AddDrawing</button>   
+                }
+            </div>
         </div>
     )
 
