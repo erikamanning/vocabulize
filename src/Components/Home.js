26a12004ed4a794896c2b95990fd30ea797c4477
dantlangston <dan.t.langston@gmail.com>
2021-11-22 15:54:11 -0500

    added animated CTA at top of home.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,13 +7,20 @@ import HomeAbout from "./Home/HomeAbout";
 
 const Home = () => {
     return(
-        <div style={{marginTop:'20%', }}>
-            <div className='Card'>               
-                <div>
-                    <h1 className='card-word home-card'>Welcome to Vocabulize</h1>
-                    <p className='definition home-card'>The best place to study for visual learners</p>
-                </div>
-            
+        <div style={{marginTop:'5%', }}>              
+                <div className='home-top'>
+                    <motion.div 
+                        initial={{opacity:0}}
+                        animate={{opacity:1}}
+                        transition={{duration:1, delay:1}}
+                        whileHover={{
+                            scale: 1.1,
+                            transition: { duration: 1, delay:0 },
+                        }}
+                        style={{margin:'0 auto 0 auto', cursor:'pointer'}}>
+                        <h1 style={{color:'var(--text-color'}}>Learn faster.</h1>
+                        <p style={{color:'var(--text-accent'}}>Scroll to learn more</p>
+                    </motion.div>
             </div>
             <HomeExplanation />
             <HomeAbout />
