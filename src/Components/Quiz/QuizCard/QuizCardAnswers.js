910bd4ca25de8565de4d2cbc879b39e5299c0e9a
erika <erikamanning@protonmail.com>
2021-11-22 14:15:03 -0800

    added separate component for individual answers

--- src/Components/Quiz/QuizCard/QuizCardAnswers.js
+++ src/Components/Quiz/QuizCard/QuizCardAnswers.js
@@ -1,18 +1,19 @@
 import React from "react"
 import { v4 as uuidv4 } from 'uuid';
-
+import Answer from './Answer'
 
 const QuizCardAnswers = ({answers}) => {
 
     // need to add shuffle answers here or before this point
+    const letters = ['A', 'B', 'C'];
 
     return (
 
         <div>
             <h5>Choose one of the following:</h5>
-            <ul>
-                {answers.map((answer)=>(<li key={uuidv4()}>{answer}</li>))}
-            </ul>
+            <div>
+                {answers.map((answer,i)=>(<Answer key={uuidv4()} letter={letters[i]} text={answer} />))}
+            </div>
         </div>
     )
 
