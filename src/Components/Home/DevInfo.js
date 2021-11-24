861e62f66e835cdea4b4a297a6e4a1b1c6346765
dantlangston <dan.t.langston@gmail.com>
2021-11-24 16:41:34 -0500

    dev cards styling and setup

--- src/Components/Home/DevInfo.js
+++ src/Components/Home/DevInfo.js
@@ -1,19 +1,35 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 import './DevInfo.css'
+import danphoto from '../../Assets/dan-dev-photo.png'
+import erikaphoto from '../../Assets/erika-dev-photo.jpg'
 
 const DevInfo = () => {
     return(
         <div className='dev-info-container'>
             <div className='dev-card'>
-                <img />
-                <p className='dev-blurb'></p>
+                <img className='dev-img' src={erikaphoto}/>
+                <p className='dev-blurb'>Erika is a developer from utah</p>
                 <div className='dev-link-container'>
-                    <a>
-                        <FontAwesomeIcon />
+                    <a href='' target='_blank'>
+                        <FontAwesomeIcon className='icon' icon={faGithubSquare}/>
                     </a>
-                    <a>
-                        <FontAwesomeIcon />
+                    <a href='' target='_blank'>
+                        <FontAwesomeIcon className='icon' icon={faLinkedin}/>
+                    </a>
+                </div>
+            </div>
+
+            <div className='dev-card'>
+                <img className='dev-img' src={erikaphoto}/>
+                <p className='dev-blurb'>Erika is a developer from utah</p>
+                <div className='dev-link-container'>
+                    <a href='' target='_blank'>
+                        <FontAwesomeIcon className='icon' icon={faGithubSquare}/>
+                    </a>
+                    <a href='' target='_blank'>
+                        <FontAwesomeIcon className='icon' icon={faLinkedin}/>
                     </a>
                 </div>
             </div>
